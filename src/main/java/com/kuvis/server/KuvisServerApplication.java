a3d838b278fa8075d4ce2a9f530259b63849ca7d
HongGit <soojam@konkuk.ac.kr>
2024-09-26 16:54:02 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/com/kuvis/server/KuvisServerApplication.java
@@ -0,0 +1,13 @@
+package com.kuvis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuvisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuvisServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuvis/server/KuvisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuvis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuvisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
